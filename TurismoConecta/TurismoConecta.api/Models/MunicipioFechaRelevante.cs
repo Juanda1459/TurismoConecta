688bfeeed9e4ed5e7f806263ece799c3e64efe71
asjulac2207-bit <asjulac2207@gmail.com>
2026-09-08 06:24:52 -0500

    Commit: Arreglos de Etiqueta, base de datos tabla FechasRelevantesMunicipioFechasRelevantes

---
+++ TurismoConecta/TurismoConecta.api/Models/MunicipioFechaRelevante.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TurismoConecta.api.Models;
+
+public partial class MunicipioFechaRelevante
+{
+    public int IdMunicipioFechaRelevante { get; set; }
+
+    public int IdMunicipio { get; set; }
+
+    public int IdFechaRelevante { get; set; }
+
+    public string? FotoUrl { get; set; }
+
+    public bool EsFestividadPrincipal { get; set; }
+
+    public string? DescripcionLocal { get; set; }
+
+    public int Orden { get; set; }
+
+    public DateTime FechaCreacion { get; set; }
+
+    public virtual FechaRelevante IdFechaRelevanteNavigation { get; set; } = null!;
+
+    public virtual Municipio IdMunicipioNavigation { get; set; } = null!;
+}
