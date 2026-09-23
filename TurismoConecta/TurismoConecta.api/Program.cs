5a014b99a58b9df1233a8061a31f4e240660c9a2
Juan Tovar <jdtovar.180@gmail.com>
2026-09-23 10:51:40 -0500

    Commit: se implemento la correccion de codificacion.

--- TurismoConecta/TurismoConecta.api/Program.cs
+++ TurismoConecta/TurismoConecta.api/Program.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
